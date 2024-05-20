765a3faf143ba8538523f37ece9f99e0554ce5c9
gabrielamayta <gabrielamayta2109@gmail.com>
2024-05-20 11:31:04 -0300

    agregue la lista de generos

---
+++ musica/genero.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template
+from . import db
+bp = Blueprint('genero', __name__,url_prefix='/genero' )
+@bp.route()
+def generos():
+    base_de_datos = db.get_db()
+    consulta = """
+        SELECT name FROM genres
+        ORDER by name;
+    """
+    resultado = base_de_datos.execute(consulta)
+    lista_de_resultado = resultado.fetchall()
+    return render_template("genero/genero.html",generos=lista_de_resultado)
+
+@bp.route('detalle/<int:id>')
+def detalle(id):
+    base_de_datos = db.get_db()
+    consulta = """
+        SELECT name FROM genres
+        ORDER by name;
+    """
+    resultado = base_de_datos.execute(consulta)
+    lista_de_resultado = resultado.fetchall()
+    return render_template("genero/genero.html",generos=lista_de_resultado)
